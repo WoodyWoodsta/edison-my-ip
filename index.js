b44f762b8249666a603fe2c0108e0834b9a725ee
Sean Wood <woodywoodsta@gmail.com>
2016-07-30 18:56:21 +0000

    add error retry, add debug logging

--- index.js
+++ index.js
@@ -8,10 +8,30 @@ var fs = require('fs');
 
 var config = JSON.parse(fs.readFileSync('./config.json'));
 
-// Fetch the IP address
-publicIp.v4().then(function(ip) {
-  pushIp(ip, config.gistId);
-});
+findIp(config.gistId);
+
+/**
+ * Recursively retry for a valid IP address
+ * @param  {string} gistId The ID of the gist to push to
+ */
+function findIp(gistId) {
+  console.log('[edison-my-ip] - Starting IP fetch...');
+  publicIp.v4()
+    .then(function(ip) {
+      console.log('[edison-my-ip] - IP fetched successfully');
+      pushIp(ip, config.gistId);
+  })
+    .catch(function(err) {
+      console.log('[edison-my-ip] - Failed to fetch IP: ', err);
+
+      // Retry
+      setTimeout(function(gistId) {
+        console.log('[edison-my-ip] - Retrying IP fetch...');
+
+        findIp(gistId);
+      }, 10000);
+  });
+}
 
 /**
  * Push the IP address to the gist with the specified ID
@@ -19,8 +39,11 @@ publicIp.v4().then(function(ip) {
  * @param  {string} gistId The ID of the gist to push to
  */
 function pushIp(ip, gistId) {
+  console.log('[edison-my-ip] - Creating client and gist objects...');
+
   var client = github.client(config.authToken);
   var gist = client.gist();
+  console.log('[edison-my-ip] - Client and gist objects created successfully');
 
   var content = {
     description: '',
@@ -30,10 +53,15 @@ function pushIp(ip, gistId) {
       }
     }
   };
+  console.log('[edison-my-ip] - Attempting to edit gist...');
 
   gist.edit(gistId, content, function(err) {
     if (err) {
+      console.log('[edison-my-ip] - Edit gist attempt failed');
+
       fs.writeFileSync('edison-my-ip-log.txt', `The push failed with error:\n${err}`);
+    } else {
+      console.log('[edison-my-ip] - Gist edit successful');
     }
   });
 }
